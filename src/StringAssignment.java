095608910f67de605411360c41e9db8c2e887f5b
canaanmcarman <canaan.m.carman@gmail.com>
2021-07-07 17:16:29 -0400

    first commit

---
+++ src/StringAssignment.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class StringAssignment {
+    public static void main(String[] args){
+            Scanner keyboard = new Scanner(System.in);
+
+            String name;
+            System.out.print("What is your name? ");
+            name = keyboard.nextLine();
+            System.out.print("Wow! " + name + " is a great name!");
+
+
+
+
+
+    }
+}
